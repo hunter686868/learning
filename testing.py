e4d83e70817a96501eb1eff2367dfe9b251e14d5
Sergei <hunter6868@ya.ru>
2023-12-03 23:26:45 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	testing.py

--- testing.py
+++ testing.py
@@ -15,7 +15,7 @@ class FuncTests(unittest.TestCase):
 
     def test_strings_empty(self):
         # Третий тест проверит, будет ли функция работать с пустыми значениями
-        self.assertTrue(func('', ''))
+        self.assertFalse(func('', ''))
 
     def test_substring_abroad(self):
         # Четвертый тест проверит, будет ли функция корректно работать, если подстрока начинает
@@ -26,5 +26,4 @@ class FuncTests(unittest.TestCase):
         # Пятый тест проверит, вернет ли функция True, если строка и подстрока равны
         self.assertTrue(func('12345', '12345'))
 
-input('Нажмите любую клавишу')
-# python -m unittest testing.py
+#
