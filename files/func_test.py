2cb5b7145eda79cb53c54c9dfa4ce44e3436f053
Sergei <hunter6868@ya.ru>
2023-12-26 16:42:47 +0300

    update

--- files/func_test.py
+++ files/func_test.py
@@ -1,6 +1,10 @@
 from files import func
 import unittest
 
-class FuncTests(unittest.TestCase):
 
-    def
+class FuncTests(unittest.TestCase):
+    def test_success(self):
+        self.assertTrue(func(2,10,'/Users/Sergei/Documents/GitHub/learning/files/'))
+
+    def test_fail(self):
+        self.assertFalse(func(2,11,'/Users/Sergei/Documents/GitHub/learning/files/'))
