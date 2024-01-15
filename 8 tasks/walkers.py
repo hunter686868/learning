b8d33b0320f77c534c3ccd47ff990489bac18f6b
Sergei <hunter6868@ya.ru>
2024-01-15 13:31:00 +0300

    update

--- 8 tasks/walkers.py
+++ 8 tasks/walkers.py
@@ -1,18 +1,25 @@
 def white_stalkers(village: str) -> bool:
-    return find_stalkers(village, 0, 0, 0)
+    if len(village) < 5:
+        return False
+    return find_stalkers(village, 0, 0, 0, 0)
 
 
-def find_stalkers(village, i, sum_d, sum_v):
+def find_stalkers(village, i, sum_d, sum_v, flag):
     if i == len(village):
+        if flag >= 1:
+            return True
         return False
     value = village[i]
-    if value.isdigit():
-        sum_d += int(value)
-    elif value == '=':
+    if sum_d and value == '=':
         sum_v += 1
-    if sum_d == 10 and sum_v == 3:
-        return True
-    return find_stalkers(village, i+1, sum_d, sum_v)
+    elif value.isdigit():
+        sum_d += int(value)
+        if sum_d == 10 and sum_v != 3:
+            return False
+        elif sum_d == 10 and sum_v == 3:
+            flag += 1
+            sum_d = int(value)
+            sum_v = 0
+    return find_stalkers(village, i+1, sum_d, sum_v, flag)
 
 
-print(white_stalkers('abc=7==hdjs=3gg1=======5'))
