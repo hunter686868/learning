4e1efac50f120f872933878199a479e250e8624d
Sergei <hunter6868@ya.ru>
2024-01-18 11:46:15 +0300

    update

--- 8 tasks/matrix.py
+++ 8 tasks/matrix.py
@@ -1,21 +1,18 @@
-def digital_rain(col: str) -> str:
-    if len(col) < 2:
-        return ''
-    return find_substring(col, 0, 0, 0, '')
+def digital_rain(col):
+    zero = 0
+    one = 0
+    max_len = 0
+    max_str = ""
 
+    for i in reversed(range(len(col))):
+        if int(col[i]) == 0:
+            zero += 1
+        else:
+            one += 1
+        if zero == one:
+            curr_len = zero + one
+            if i + curr_len > max_len:
+                max_len = curr_len
+                max_str = col[i:i + curr_len]
 
-def find_substring(col, i, zero, one, max_len):
-    if len(col) == i:
-        return max_len
-    value = col[i]
-    if int(value) == 0:
-        zero += 1
-    else:
-        one += 1
-    if zero == one:
-        cur_len = zero + one
-        max_len = col[i - cur_len + 1:i + 1]
-    return find_substring(col, i + 1, zero, one, max_len)
-
-
-print(digital_rain('011111110'))
+    return max_str
