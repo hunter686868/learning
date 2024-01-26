28a7e7b649e1c0e1db4fb179a9cedc71e2ba37b6
Sergei <hunter6868@ya.ru>
2024-01-26 10:31:15 +0300

    update

--- 8 tasks/cyberpunk_1992.py
+++ 8 tasks/cyberpunk_1992.py
@@ -1,28 +1,5 @@
 def massdriver(activate: list) -> int:
-    dct = {}
-    lst = []
-    for i in range(len(activate)):
-        if activate[i] in dct:
-            lst.append(dct[activate[i]])
-        else:
-            dct[activate[i]] = i
-    if lst:
-        return min(lst)
-    return -1
-
-
-print(massdriver([1,2,3,1,2,3,4]))# = 0
-print(massdriver([1,2,3,4,3,4,2]))# = 1
-print(massdriver([1,2,3,4,5,6,7]))# = -1)
-
-
-def massdriver(activate: list) -> int:
     for i in range(len(activate)):
         if activate.count(activate[i]) > 1:
             return i
     return -1
-
-
-print(massdriver([1,2,3,1,2,3,4]))# = 0
-print(massdriver([1,2,3,4,3,4,2]))# = 1
-print(massdriver([1,2,3,4,5,6,7]))# = -1)
