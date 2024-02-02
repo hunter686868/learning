a744836c5be673bd86c2f08ea904715a6988ef50
Sergei <hunter6868@ya.ru>
2024-02-03 01:39:15 +0300

    update

---
+++ 8 tasks/cyberpunk_1994.py
@@ -0,0 +1,22 @@
+def tr_matrix(top, bottom, left, right, mx, result):
+    if top > bottom or left > right:
+        return result
+    for i in range(left, right + 1):
+        result.append(mx[top][i])
+    top += 1
+    for i in range(top, bottom + 1):
+        result.append(mx[i][right])
+    right -= 1
+    if top <= bottom:
+        for i in reversed(range(left, right + 1)):
+            result.append(mx[bottom][i])
+        bottom -= 1
+    if left <= right:
+        for i in reversed(range(top, bottom + 1)):
+            result.append(mx[i][left])
+        left += 1
+    return tr_matrix(top, bottom, left, right, mx, result)
+
+
+def matrix(n, m, matrix):
+    return tr_matrix(0, m - 1, 0, n - 1, matrix, [])
