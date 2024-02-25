3eb74e4019baaf356cd3c2b4417f9db310808bee
hunter <hunter6868@ya.ru>
2024-02-25 14:02:09 +0300

    update

--- ASD/linked_list2.py
+++ ASD/linked_list2.py
@@ -20,22 +20,87 @@ class LinkedList2:
         self.tail = item
 
     def find(self, val):
-        return None # здесь будет ваш код
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
 
     def find_all(self, val):
-        return [] # здесь будет ваш код
+        nodes = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                nodes.append(node)
+            node = node.next
+        return nodes
 
     def delete(self, val, all=False):
-        pass # здесь будет ваш код
+        if not self.head:
+            return
+        node = self.head
+        i = node.prev
+        while node is not None:
+            if node.value == val:
+                if i is None:
+                    self.head = node.next
+                    self.head.prev = None
+                else:
+                    i.next = node.next
+                    i.prev = node.prev
+                if node == self.tail:
+                    self.tail = i
+                    i.prev = node.prev
+                    i.next = None
+                if not all:
+                    return
+            else:
+                i = node
+            node = node.next
 
     def clean(self):
-        pass # здесь будет ваш код
+        self.head = None
+        self.tail = None
 
     def len(self):
-        return 0 # здесь будет ваш код
+        count = 0
+        node = self.head
+        while node is not None:
+            count += 1
+            node = node.next
+        return count
 
     def insert(self, afterNode, newNode):
-        pass # здесь будет ваш код
+        if afterNode is None:
+            if self.head is None and self.tail is None:
+                newNode.next = None
+                newNode.prev = None
+                self.head = newNode
+                self.tail = self.head
+            else:
+                self.tail.next = newNode
+                newNode.prev = self.tail
+                self.tail = newNode
+                self.tail.next = None
+        else:
+            if afterNode.next is None:
+                afterNode.next = newNode
+                newNode.prev = afterNode
+                self.tail = newNode
+                self.tail.next = None
+            else:
+                afterNode.next.prev = newNode
+                newNode.next = afterNode.next
+                afterNode.next = newNode
 
     def add_in_head(self, newNode):
-        pass # здесь будет ваш код
+        if self.tail is None:
+            self.tail = newNode
+            newNode.next = None
+            newNode.prev = None
+        else:
+            self.head.prev = newNode
+            newNode.next = self.head
+        self.head = newNode
+
