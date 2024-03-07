b77548128a6cead8b006da1074a278e2477ff371
Sergei <hunter6868@ya.ru>
2024-03-07 17:46:18 +0300

    update

--- ASD/queue.py
+++ ASD/queue.py
@@ -2,12 +2,18 @@ class Queue:
     def __init__(self):
         self.queue = []
 
-    def enqueue(self, item):
+    def enqueue(self, item): # O(1)
         self.queue = self.queue + [item]
 
-    def dequeue(self):
+    def dequeue(self): # O(n)
         if self.size() == 0:
             return None
+        value = self.queue[0]
+        new_queue = []
+        for i in range(1, self.size()):
+            new_queue.append(self.queue[i])
+        self.queue = new_queue
+        return value
 
     def size(self):
         if not self.queue:
@@ -16,3 +22,33 @@ class Queue:
         for _ in self.queue:
             cnt += 1
         return cnt
+
+    def rotation(self, n):
+        if self.size() == 0:
+            raise IndexError('Nothing to rotate')
+        for _ in range(n):
+            val = self.dequeue()
+            self.enqueue(val)
+
+from stack import Stack
+
+class Queue_stack:
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, item):
+        self.stack1.push(item)
+
+    def dequeue(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.push(self.stack1.pop())
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            return None
+
+    def size(self):
+        return self.stack1.size() + self.stack2.size()
+
---
+++ ASD/test_queue.py
@@ -0,0 +1,23 @@
+import unittest
+from queue import Queue, Queue_stack
+
+class TestsLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.queue1 = Queue()
+        self.queue2 = Queue_stack()
+    def test_enqueue(self):
+        self.queue1.enqueue(1)
+        self.queue2.enqueue(1)
+        self.assertEqual(self.queue2.size(), 1)
+        self.assertEqual(self.queue1.size(), 1)
+    def test_dequeue(self):
+        self.queue1.enqueue(1)
+        self.queue2.enqueue(1)
+        self.queue1.dequeue()
+        self.queue2.dequeue()
+        self.assertEqual(self.queue2.size(), 0)
+        self.assertEqual(self.queue1.size(), 0)
+    def test_size(self):
+        self.queue2.enqueue(1)
+        self.assertEqual(self.queue2.size(), 1)
+        self.assertEqual(self.queue1.size(), 0)
