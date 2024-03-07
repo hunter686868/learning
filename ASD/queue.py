c7125985ecbe93be3976127660e76a5569b007d9
Sergei <hunter6868@ya.ru>
2024-03-07 18:33:32 +0300

    update

--- ASD/queue.py
+++ ASD/queue.py
@@ -33,26 +33,5 @@ class Queue:
             val = self.dequeue()
             self.enqueue(val)
 
-#from stack import Stack
-#
-#class Queue_stack:
-#    def __init__(self):
-#       self.stack1 = Stack()
-#        self.stack2 = Stack()
-#
-#    def enqueue(self, item):
-#        self.stack1.push(item)
-#
-#    def dequeue(self):
-#        if not self.stack2:
-#            while self.stack1:
-#                self.stack2.push(self.stack1.pop())
-#        if self.stack2:
-#            value = self.stack2.pop()
-#            return value
-#        else:
-#            return None
 
-#    def size(self):
-#        return self.stack1.size() + self.stack2.size()
 
