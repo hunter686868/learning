ae967f59bcf0aeacfbd509831b4b949da9105d08
hunter <hunter6868@ya.ru>
2024-03-08 12:36:29 +0300

    update

--- ASD/test_queue.py
+++ ASD/test_queue.py
@@ -1,5 +1,5 @@
 import unittest
-from queue import Queue
+from queues import Queue
 
 
 class TestsLinkedList(unittest.TestCase):
@@ -8,10 +8,14 @@ class TestsLinkedList(unittest.TestCase):
 
     def test_enqueue(self):
         self.queue1.enqueue(1)
-        self.assertEqual(self.queue1.size(), 1)
+        self.queue1.enqueue(1)
+        self.queue1.enqueue(1)
+        self.assertEqual(self.queue1.size(), 3)
 
     def test_dequeue(self):
         self.queue1.enqueue(1)
+        self.queue1.dequeue()
+        self.queue1.dequeue()
         self.queue1.dequeue()
         self.assertEqual(self.queue1.size(), 0)
 
@@ -27,3 +31,11 @@ class TestsLinkedList(unittest.TestCase):
         self.assertEqual(self.queue1.dequeue(), 3)
         self.assertEqual(self.queue1.dequeue(), 1)
         self.assertEqual(self.queue1.dequeue(), 2)
+        self.queue1.enqueue(1)
+        self.queue1.enqueue(2)
+        self.queue1.enqueue(3)
+        self.queue1.enqueue(4)
+        self.queue1.enqueue(5)
+        self.queue1.enqueue(6)
+        self.queue1.rotate(10)
+        self.assertEqual(self.queue1.dequeue(), 5)
