ef3568b658b8ad50aefff2841bfd08bd26209294
Sergei <hunter6868@ya.ru>
2024-03-11 17:59:50 +0300

    update

--- ASD/test_deque.py
+++ ASD/test_deque.py
@@ -1,30 +1,27 @@
 import unittest
 from deque import Deque
 
+
 class TestDequeMethods(unittest.TestCase):
+    def setUp(self):
+        self.deq = Deque()
+        self.deq.addFront(1)
+        self.deq.addTail(2)
 
     def test_addFront(self):
-        deq = Deque()
-        deq.addFront("f1")
-        self.assertEqual(deq.size(), 1)
-        self.assertEqual(deq.removeFront(), "f1")
+        self.assertEqual(self.deq.size(), 2)
+        self.assertEqual(self.deq.removeFront(), "f1")
 
     def test_addTail(self):
-        deq = Deque()
-        deq.addTail("t1")
-        self.assertEqual(deq.size(), 1)
-        self.assertEqual(deq.removeTail(), "t1")
+        self.deq.addTail("t1")
+        self.assertEqual(self.deq.size(), 1)
+        self.assertEqual(self.deq.removeTail(), "t1")
 
     def test_removeFront(self):
-        deq = Deque()
-        deq.addFront("f1")
-        deq.addTail("t1")
-        self.assertEqual(deq.removeFront(), "f1")
-        self.assertEqual(deq.size(), 1)
+        self.assertEqual(self.deq.removeFront(), "f1")
+        self.assertEqual(self.deq.size(), 1)
 
     def test_removeTail(self):
-        deq = Deque()
-        deq.addFront("f1")
-        deq.addTail("t1")
-        self.assertEqual(deq.removeTail(), "t1")
-        self.assertEqual(deq.size(), 1)
+        self.assertEqual(self.deq.removeTail(), "t1")
+        self.assertEqual(self.deq.size(), 1)
+
