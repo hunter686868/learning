838fcbe1e684f1c5f2828a9f30cc02f0adf903ff
Sergei <hunter6868@ya.ru>
2024-03-14 17:31:08 +0300

    update

--- ASD/ordered_list.py
+++ ASD/ordered_list.py
@@ -4,6 +4,7 @@ class Node:
         self.prev = None
         self.next = None
 
+
 class OrderedList:
     def __init__(self, asc):
         self.head = None
@@ -11,28 +12,90 @@ class OrderedList:
         self.__ascending = asc
 
     def compare(self, v1, v2):
+        if v1 < v2:
+            return -1
+        elif v1 > v2:
+            return 1
         return 0
-        # -1 если v1 < v2
-        # 0 если v1 == v2
-        # +1 если v1 > v2
 
     def add(self, value):
-        pass
-        # автоматическая вставка value
-        # в нужную позицию
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        if self.compare(value, self.head.value) == -1:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            return
+        if self.compare(value, self.tail.value) == 1:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            return
+        curr_node = self.head
+        while curr_node is not None:
+            comparison = self.compare(value, curr_node.value)
+            if comparison == 0:
+                new_node.prev = curr_node
+                new_node.next = curr_node.next
+                if curr_node.next is not None:
+                    curr_node.next.prev = new_node
+                curr_node.next = new_node
+                return
+            elif comparison == 1 and self.__ascending:
+                new_node.prev = curr_node
+                new_node.next = curr_node.next
+                if curr_node.next is not None:
+                    curr_node.next.prev = new_node
+                curr_node.next = new_node
+                return
+            elif comparison == -1 and not self.__ascending:
+                new_node.next = curr_node
+                new_node.prev = curr_node.prev
+                curr_node.prev.next = new_node
+                curr_node.prev = new_node
+                return
+            curr_node = curr_node.next
 
     def find(self, val):
-        return None # здесь будет ваш код
+        curr_node = self.head
+        while curr_node is not None:
+            comparison = self.compare(val, curr_node.value)
+            if comparison == 0:
+                return curr_node
+            elif (self.__ascending and comparison == 1) or (not self.__ascending and comparison == -1):
+                return None
+            curr_node = curr_node.next
 
     def delete(self, val):
-        pass # здесь будет ваш код
+        curr_node = self.head
+        while curr_node is not None:
+            if curr_node.value == val:
+                if curr_node.prev is not None:
+                    curr_node.prev.next = curr_node.next
+                else:
+                    self.head = curr_node.next
+
+                if curr_node.next is not None:
+                    curr_node.next.prev = curr_node.prev
+                else:
+                    self.tail = curr_node.prev
+                return
+            curr_node = curr_node.next
 
     def clean(self, asc):
         self.__ascending = asc
-        pass # здесь будет ваш код
+        self.head = None
+        self.tail = None
 
     def len(self):
-        return 0 # здесь будет ваш код
+        count = 0
+        curr_node = self.head
+        while curr_node is not None:
+            count += 1
+            curr_node = curr_node.next
+        return count
 
     def get_all(self):
         r = []
@@ -42,10 +105,18 @@ class OrderedList:
             node = node.next
         return r
 
+
 class OrderedStringList(OrderedList):
     def __init__(self, asc):
         super(OrderedStringList, self).__init__(asc)
 
     def compare(self, v1, v2):
-        # переопределённая версия для строк
-        return 0
+        v1_s = v1.strip()
+        v2_s = v2.strip()
+        if v1_s < v2_s:
+            return -1
+        elif v1_s == v2_s:
+            return 0
+        else:
+            return 1
+
---
+++ ASD/test_ordered_list.py
@@ -0,0 +1,38 @@
+import unittest
+from ordered_list import OrderedList, OrderedStringList
+class TestOrderedList(unittest.TestCase):
+    def test_add(self):
+        # Тестирование добавления элементов в упорядоченный список по возрастанию
+        ordered_list_asc = OrderedList(asc=True)
+        ordered_list_asc.add(3)
+        ordered_list_asc.add(1)
+        ordered_list_asc.add(2)
+        self.assertEqual([node.value for node in ordered_list_asc.get_all()], [1, 2, 3])
+
+        # Тестирование добавления элементов в упорядоченный список по убыванию
+        ordered_list_desc = OrderedList(asc=False)
+        ordered_list_desc.add(3)
+        ordered_list_desc.add(1)
+        ordered_list_desc.add(2)
+        self.assertEqual([node.value for node in ordered_list_desc.get_all()], [3, 2, 1])
+
+    def test_delete(self):
+        ordered_list = OrderedList(asc=True)
+        ordered_list.add(3)
+        ordered_list.add(1)
+        ordered_list.add(2)
+        ordered_list.delete(1)
+        self.assertEqual([node.value for node in ordered_list.get_all()], [2, 3])
+        ordered_list.delete(3)
+        self.assertEqual([node.value for node in ordered_list.get_all()], [2])
+        ordered_list.delete(2)
+        self.assertEqual([node.value for node in ordered_list.get_all()], [])
+
+    def test_find(self):
+        ordered_list = OrderedList(asc=True)
+        ordered_list.add(3)
+        ordered_list.add(1)
+        ordered_list.add(2)
+        self.assertEqual(ordered_list.find(1).value, 1)
+        self.assertEqual(ordered_list.find(4), None)
+        self.assertEqual(ordered_list.find(2).value, 2)
