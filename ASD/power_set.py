8ea9f4dd2d8efe0cb46fd8cde6bb2b8922bb0dc7
Sergei <hunter6868@ya.ru>
2024-03-24 15:01:54 +0300

    update

---
+++ ASD/power_set.py
@@ -0,0 +1,42 @@
+# наследуйте этот класс от HashTable
+# или расширьте его методами из HashTable
+from hash_table import HashTable
+class PowerSet(HashTable):
+
+    #def __init__(self):
+        # ваша реализация хранилища
+
+    def size(self):
+        return self.size()
+        # количество элементов в множестве
+
+    def put(self, value):
+        # всегда срабатывает
+
+    def get(self, value):
+        # возвращает True если value имеется в множестве,
+        # иначе False
+        return False
+
+    def remove(self, value):
+        # возвращает True если value удалено
+        # иначе False
+        return False
+
+    def intersection(self, set2):
+        # пересечение текущего множества и set2
+        return None
+
+    def union(self, set2):
+        # объединение текущего множества и set2
+        return None
+
+    def difference(self, set2):
+        # разница текущего множества и set2
+        return None
+
+    def issubset(self, set2):
+        # возвращает True, если set2 есть
+        # подмножество текущего множества,
+        # иначе False
+        return False
