91aeffd255b5202bfe75bb6a0c96912930c67ac2
Sergei <hunter6868@ya.ru>
2024-06-22 20:40:34 +0300

    update

--- asd_2/balanced_binary_trees.py
+++ asd_2/balanced_binary_trees.py
@@ -1,19 +1,14 @@
 def GenerateBBSTArray(a):
-    if a is None:
+    if not a:
         return []
     a.sort()
+
     def create_bbst(a, start, end):
         if start > end:
             return []
         middle_index = (start + end)//2
         middle_item = a[middle_index]
-        left_subtree = create_bbst(a, start, middle_item - 1)
-        right_subtree = create_bbst(a, middle_item + 1, end)
+        left_subtree = create_bbst(a, start, middle_index - 1)
+        right_subtree = create_bbst(a, middle_index + 1, end)
         return [middle_item] + left_subtree + right_subtree
     return create_bbst(a, 0, len(a) - 1)
-
-
-
-a = [2, 1, 3, 5, 4, 6, 9, 7, 8, 0]
-b = GenerateBBSTArray(a)
-print(b)
