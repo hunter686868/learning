e208c505abb920ae23f267e875df3729c4ae0b56
Sergei <hunter6868@ya.ru>
2024-06-22 20:41:47 +0300

    update

--- asd_2/balanced_binary_trees.py
+++ asd_2/balanced_binary_trees.py
@@ -12,3 +12,4 @@ def GenerateBBSTArray(a):
         right_subtree = create_bbst(a, middle_index + 1, end)
         return [middle_item] + left_subtree + right_subtree
     return create_bbst(a, 0, len(a) - 1)
+
