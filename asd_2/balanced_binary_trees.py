6aa5c0d08e14d2a4708920a4159cebccfc88a447
Sergei <hunter6868@ya.ru>
2024-06-22 20:35:57 +0300

    update

--- asd_2/balanced_binary_trees.py
+++ asd_2/balanced_binary_trees.py
@@ -1,6 +1,16 @@
 def GenerateBBSTArray(a):
-    a = a.sort()
-    return a
+    if a is None:
+        return []
+    a.sort()
+    def create_bbst(a, start, end):
+        if start > end:
+            return []
+        middle_index = (start + end)//2
+        middle_item = a[middle_index]
+        left_subtree = create_bbst(a, start, middle_item - 1)
+        right_subtree = create_bbst(a, middle_item + 1, end)
+        return [middle_item] + left_subtree + right_subtree
+    return create_bbst(a, 0, len(a) - 1)
 
 
 
--- asd_2/test_balanced_binary_trees.py
+++ asd_2/test_balanced_binary_trees.py
@@ -1,6 +1,24 @@
 from balanced_binary_trees import GenerateBBSTArray
 import unittest
 
-class test_balanced_tree(unittest.TestCase):
-    def SetUp(self):
-        a = [2, 1, 3, 5, 4, 6, 9, 7, 8, 0]
+
+class TestGenerateBBSTArray(unittest.TestCase):
+    def test_sorted_input(self):
+        self.assertEqual(GenerateBBSTArray([1, 2, 3, 4, 5]), [3, 1, 2, 4, 5])
+
+    def test_reverse_sorted_input(self):
+        self.assertEqual(GenerateBBSTArray([5, 4, 3, 2, 1]), [3, 1, 2, 4, 5])
+
+    def test_unsorted_input(self):
+        self.assertEqual(GenerateBBSTArray([3, 1, 2, 5, 4]), [3, 1, 2, 4, 5])
+
+    def test_single_element(self):
+        self.assertEqual(GenerateBBSTArray([1]), [1])
+
+    def test_empty_array(self):
+        self.assertEqual(GenerateBBSTArray([]), [])
+
+    def test_large_input(self):
+        input_array = list(range(1, 16))  # Дерево глубины 3 (15 элементов)
+        expected_output = [8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15]
+        self.assertEqual(GenerateBBSTArray(input_array), expected_output)
