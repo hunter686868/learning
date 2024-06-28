bd672def858359460fd79e37c82687b08d239361
Sergei <hunter6868@ya.ru>
2024-06-28 11:53:49 +0300

    update

--- asd_2/heap.py
+++ asd_2/heap.py
@@ -5,14 +5,41 @@ class Heap:
 
     def MakeHeap(self, a, depth):
         # создаём массив кучи HeapArray из заданного
-        heap =
+        heap_size = 2 ** (depth + 1) - 1
+        self.HeapArray = [None] * heap_size
+
+        for item in a:
+            self.Add(item)
         # размер массива выбираем на основе глубины depth
-        pass
 
     def GetMax(self):
+        if self.HeapArray is None or self.HeapArray[0] is None:
         # вернуть значение корня и перестроить кучу
-        return -1  # если куча пуста
+            return -1  # если куча пуста
+
+        max_value = self.HeapArray[0]
+
+        for index in range(1, len(self.HeapArray)):
+            self.HeapArray[index - 1] = self.HeapArray[index]
+        self.HeapArray.pop()
+        return max_value
 
     def Add(self, key):
         # добавляем новый элемент key в кучу и перестраиваем её
-        return False  # если куча вся заполнена
+        if self.HeapArray and None not in self.HeapArray:
+            return False  # если куча вся заполнена
+
+        index = 0
+
+        while index < len(self.HeapArray) and self.HeapArray[index] is not None:
+            index += 1
+
+        self.HeapArray[index] = key
+
+        parent_index = (index - 1) // 2
+        while index > 0 and self.HeapArray[parent_index] < self.HeapArray[index]:
+            self.HeapArray[parent_index] = self.HeapArray[index]
+            self.HeapArray[index] = self.HeapArray[parent_index]
+            index = parent_index
+            parent_index = (index - 1) // 2
+        return True
