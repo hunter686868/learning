07b960eaa09b9db137d6694811b44e91bf70c396
Sergei <hunter6868@ya.ru>
2025-02-10 10:26:28 +0300

    update

--- OOAP_1/bounded_stack.py
+++ OOAP_1/bounded_stack.py
@@ -1,2 +1,28 @@
 class BoundedStack:
     def __init__(self):
+        self.items = []
+        self.top = None
+        self.is_empty = True
+
+    def push(self, item):
+        self.items.append(item)
+        self.is_empty = False
+        self.top = item
+    def pop(self):
+        if self.is_empty:
+            raise IndexError("Stack is empty")
+        else:
+            self.is_empty = False
+            self.top = self.items.pop()
+            self.is_empty = True
+            return self.top
+    def peek(self):
+        if self.is_empty:
+            raise IndexError("Stack is empty")
+        else:
+            self.is_empty = False
+            self.top = self.items[-1]
+            self.is_empty = True
+            return self.top
+
+
