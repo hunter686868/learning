de1fa58653410799eb5ddef0315350cac7471d8c
Sergei <hunter6868@ya.ru>
2025-12-25 10:03:16 +0300

    update

--- java_oop/LinkedList.java
+++ java_oop/LinkedList.java
@@ -1,5 +1,4 @@
 import java.util.*;
-// Рефлексию как понял необходимо писать по эталонным решениям, поэтому буду писать со следующего задания.
 
 public class LinkedList
 {
@@ -12,17 +11,21 @@ public class LinkedList
         tail = null;
     }
 
-    public void addInTail(Node item) {
+    public void addInTail(Node item)
+    {
         if (this.head == null)
             this.head = item;
         else
             this.tail.next = item;
+
         this.tail = item;
     }
 
-    public Node find(int value) {
+    public Node find(int value)
+    {
         Node node = this.head;
-        while (node != null) {
+        while (node != null)
+        {
             if (node.value == value)
                 return node;
             node = node.next;
@@ -30,32 +33,39 @@ public class LinkedList
         return null;
     }
 
-    // (4) Поиск всех узлов по значению. Сложность 0(n)
-    public ArrayList<Node> findAll(int _value) {
+    // (4) Поиск всех узлов по значению. O(n)
+    public ArrayList<Node> findAll(int _value)
+    {
         ArrayList<Node> nodes = new ArrayList<>();
         Node node = this.head;
-        while (node != null) {
-            if (node.value == _value) nodes.add(node);
+        while (node != null)
+        {
+            if (node.value == _value)
+                nodes.add(node);
             node = node.next;
         }
         return nodes;
     }
 
-    // (1) Удаление одного (первого) узла по значению. Сложность 0(n)
+    // (1) Удаление одного (первого) узла по значению. O(n)
     public boolean remove(int _value)
     {
         if (head == null) return false;
 
-        if (head.value == _value) {
+        if (head.value == _value)
+        {
             head = head.next;
             if (head == null) tail = null;
             return true;
         }
+
         Node prev = head;
         Node cur = head.next;
 
-        while (cur != null) {
-            if (cur.value == _value) {
+        while (cur != null)
+        {
+            if (cur.value == _value)
+            {
                 prev.next = cur.next;
                 if (cur == tail) tail = prev;
                 return true;
@@ -66,14 +76,14 @@ public class LinkedList
         return false;
     }
 
-    // (2) Удаление всех узлов по значению. Сложность 0(n)
+    // (2) Удаление всех узлов по значению. O(n)
     public void removeAll(int _value)
     {
-        while (head != null && head.value == _value) {
+        while (head != null && head.value == _value)
             head = head.next;
-        }
 
-        if (head == null) {
+        if (head == null)
+        {
             tail = null;
             return;
         }
@@ -81,51 +91,57 @@ public class LinkedList
         Node prev = head;
         Node cur = head.next;
 
-        while (cur != null) {
-            if (cur.value == _value) {
+        while (cur != null)
+        {
+            if (cur.value == _value)
+            {
                 prev.next = cur.next;
                 if (cur == tail) tail = prev;
                 cur = prev.next;
-            } else {
+            }
+            else
+            {
                 prev = cur;
                 cur = cur.next;
             }
         }
     }
 
-    // (3) Очистка списка. Сложность 0(1)
+    // (3) Очистка списка. O(1)
     public void clear()
     {
         head = null;
         tail = null;
     }
 
-    // (5) Длина списка. Сложность 0(n)
+    // (5) Длина списка. O(n)
     public int count()
-    {
-        return 0; // здесь будет ваш код подсчёта количества элементов в списке
-    }
     {
         int c = 0;
         Node node = head;
-        while (node != null) {
+        while (node != null)
+        {
             c++;
             node = node.next;
         }
         return c;
     }
 
-    // (6) Вставка после заданного узла. Сложность 0(1)
+    // (6) Вставка после заданного узла. O(1)
     public void insertAfter(Node _nodeAfter, Node _nodeToInsert)
     {
         if (_nodeToInsert == null) return;
 
-        if (_nodeAfter == null) {
-            if (head == null) {
+        if (_nodeAfter == null)
+        {
+            if (head == null)
+            {
                 head = _nodeToInsert;
                 tail = _nodeToInsert;
                 _nodeToInsert.next = null;
-            } else {
+            }
+            else
+            {
                 _nodeToInsert.next = head;
                 head = _nodeToInsert;
             }
@@ -135,9 +151,8 @@ public class LinkedList
         _nodeToInsert.next = _nodeAfter.next;
         _nodeAfter.next = _nodeToInsert;
 
-        if (tail == _nodeAfter) {
+        if (tail == _nodeAfter)
             tail = _nodeToInsert;
-        }
     }
 }
 
@@ -145,6 +160,7 @@ class Node
 {
     public int value;
     public Node next;
+
     public Node(int _value)
     {
         value = _value;
