f3117f0cb926f1f2a3a8f899f60513e58f5b6b20
Sergei <hunter6868@ya.ru>
2026-01-21 11:37:34 +0300

    update

---
+++ java_oop/Stek.java
@@ -0,0 +1,35 @@
+
+// Стек
+
+import java.util.*;
+
+public class Stack<T>
+{
+    public Stack()
+    {
+        // инициализация внутреннего хранилища стека
+    }
+
+    public int size()
+    {
+        // размер текущего стека
+        return 0;
+    }
+
+    public T pop()
+    {
+        // ваш код
+        return null;  // если стек пустой
+    }
+
+    public void push(T val)
+    {
+        // ваш код
+    }
+
+    public T peek()
+    {
+        // ваш код
+        return null; // если стек пустой
+    }
+}
