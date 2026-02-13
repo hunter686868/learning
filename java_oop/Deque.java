81b87f005c7e58bf68206f5f994d3bb1bbdbf067
Sergei <hunter6868@ya.ru>
2026-02-14 00:51:08 +0300

    update

---
+++ java_oop/Deque.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Deque<T>
+{
+    public Deque()
+    // Сложность
+    {
+        // инициализация внутреннего хранилища
+    }
+
+    public void addFront(T item)
+    // Сложность
+    {
+        // добавление в голову
+    }
+
+    public void addTail(T item)
+    // Сложность
+    {
+        // добавление в хвост
+    }
+
+    public T removeFront()
+    // Сложность
+    {
+        // удаление из головы
+        return null;
+    }
+
+    public T removeTail()
+    // Сложность
+    {
+        // удаление из хвоста
+        return null;
+    }
+
+    public int size()
+    // Сложность
+    {
+        return 0; // размер очереди
+    }
+}
+
+// Рефлексия по предыдущим задачам:
+//
